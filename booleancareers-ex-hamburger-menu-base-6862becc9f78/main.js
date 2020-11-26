ce31bf61c173ce58a2e49788d308dd8c87af6a05
piermario <piermario.salis@aol.com>
2020-11-26 22:44:33 +0100

    End

---
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,15 @@
+// JS Hamburger-Menu
+
+menuRight.click(function () {
+    HbMenu.addClass("active");
+});
+
+menuRight.click(function () {
+    HbMenu.removeClass("active");
+});
+
+
+
+var menuRight = $(".header-right > a");
+var HbMenu = $(".hamburger-menu");
+var Aselect = $(".hamburger-menu > a");
